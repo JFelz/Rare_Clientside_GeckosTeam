008877f77c355af2ca533fe282143273e2f0f8b6
Jovanni Feliz <feliz.jovanni@gmail.com>
2023-09-21 20:11:21 -0500

    Add user details

--- pages/profile.js
+++ pages/profile.js
@@ -8,9 +8,14 @@ export default function UserProfile() {
 
   return (
     <>
-      <h1>{user.displayName}</h1>
-      <h3>{user.uid}</h3>
-      <h1>{user.email}</h1>
+      <div>
+        <h1>{user.displayName}</h1>
+      </div>
+      <h4>Contact Details</h4>
+      <h5>Email</h5>
+      <p>{user.email}</p>
+      <h5>Phone Number</h5>
+      <p>{user.phoneNumber ? user.phoneNumber : 'Not available'}</p>
     </>
   );
 }
