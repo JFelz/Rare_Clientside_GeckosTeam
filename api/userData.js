b18e1bcd7771122ba25a7063ec2cb06b56263aec
Jovanni Feliz <feliz.jovanni@gmail.com>
2023-09-21 19:11:45 -0500

    stage

---
+++ api/userData.js
@@ -0,0 +1,30 @@
+// import React from 'react';
+
+// const getUser = (uid) => new Promise((resolve, reject) => {
+//   fetch(`https://localhost:7284/getuser/${uid}`, {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Accept: 'application/json',
+//     },
+//   })
+//     .then((resp) => resolve(resp.json()))
+//     .catch(reject);
+// });
+
+// const placeHolder = (uid) => new Promise((resolve, reject) => {
+//   fetch(`${clientCredentials.databaseURL}/getuser/${uid}`, {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       Accept: 'application/json',
+//     },
+//   })
+//     .then((resp) => resolve(resp.json()))
+//     .catch(reject);
+// });
+
+// export {
+//   getUser,
+//   placeHolder,
+// };
--- pages/profile.js
+++ pages/profile.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { useAuth } from '../utils/context/authContext';
 
-export default function DeleteMe() {
-  return <div>DeleteMe</div>;
+export default function UserProfile() {
+  const { user } = useAuth();
+
+  console.log(user);
+
+  return (
+    <>
+      <h1>{user.displayName}</h1>
+      <h3>{user.uid}</h3>
+      <h1>{user.email}</h1>
+    </>
+  );
 }
