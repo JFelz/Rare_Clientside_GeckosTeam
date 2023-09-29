c540622ce081a8b6c9ef21468ad36f6292ee8a6e
Sean Bryant <smbryant1730@gmail.com>
2023-09-29 17:51:00 -0400

    added create new tag functionality

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,15 +1,18 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import {
-  Navbar, //
-  Container,
-  Nav,
-  Button,
+  Navbar, Container, Nav, Button, Modal,
 } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
+import TagForm from './forms/TagForm';
 
 export default function NavBar() {
+  const [showCreateTagModal, setShowCreateTagModal] = useState(false);
+
+  const handleCloseCreateTagModal = () => {
+    setShowCreateTagModal(false);
+  };
+
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
@@ -19,7 +22,6 @@ export default function NavBar() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            {/* CLOSE NAVBAR ON LINK SELECTION: https://stackoverflow.com/questions/72813635/collapse-on-select-react-bootstrap-navbar-with-nextjs-not-working */}
             <Link passHref href="/">
               <Nav.Link>Home</Nav.Link>
             </Link>
@@ -29,6 +31,9 @@ export default function NavBar() {
             <Link passHref href="/categories">
               <Nav.Link>Category Manager</Nav.Link>
             </Link>
+            <Button variant="primary" onClick={() => setShowCreateTagModal(true)}>
+              Create Tag
+            </Button>
             <Link passHref href="/profile">
               <Nav.Link>My Profile</Nav.Link>
             </Link>
@@ -38,6 +43,15 @@ export default function NavBar() {
           </Nav>
         </Navbar.Collapse>
       </Container>
+
+      <Modal show={showCreateTagModal} onHide={handleCloseCreateTagModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Create Tag</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <TagForm />
+        </Modal.Body>
+      </Modal>
     </Navbar>
   );
 }
