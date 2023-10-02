8bdab393b1d0f671a2e13872cbb9bb089fb311b6
Jovanni Feliz <feliz.jovanni@gmail.com>
2023-10-02 17:48:00 -0500

    CRUD on Reactions

--- api/reactionData.js
+++ api/reactionData.js
@@ -22,7 +22,49 @@ const getSingleReaction = (Id) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
-const postReaction = (payload) => new Promise((resolve, reject) => {
+const getIdCount = (PostId) => new Promise((resolve, reject) => {
+  fetch(`https://localhost:7284/post/postreaction/${PostId}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+const getUserPostReaction = (UserId) => new Promise((resolve, reject) => {
+  fetch(`https://localhost:7284/posts/user/${UserId}/reactions/`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+const updateUserPostReaction = (id, payload) => new Promise((resolve, reject) => {
+  fetch(`https://localhost:7284/postreaction/update/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then(async (res) => {
+      let data;
+      if (res.ok) {
+        data = await res.json();
+        resolve(data);
+      }
+    })
+    .catch(reject);
+});
+
+const postNewReaction = (payload) => new Promise((resolve, reject) => {
   fetch('https://localhost:7284/post/postreaction', {
     method: 'POST',
     headers: {
@@ -30,13 +72,21 @@ const postReaction = (payload) => new Promise((resolve, reject) => {
     },
     body: JSON.stringify(payload),
   })
-    .then((response) => response.json())
-    .then((data) => resolve(Object.values(data)))
+    .then(async (res) => {
+      let data;
+      if (res.ok) {
+        data = await res.json();
+        resolve(data);
+      }
+    })
     .catch(reject);
 });
 
 export {
   getAllreactions,
+  updateUserPostReaction,
+  getIdCount,
   getSingleReaction,
-  postReaction,
+  getUserPostReaction,
+  postNewReaction,
 };
--- components/reactionCard.js
+++ components/reactionCard.js
@@ -2,49 +2,63 @@ import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Card } from 'react-bootstrap';
 import { useAuth } from '../utils/context/authContext';
-// import { checkUser, registerUser } from '../utils/auth';
+import { getSingleUser } from '../api/userData';
+import {
+  getIdCount,
+  getUserPostReaction,
+  postNewReaction,
+  updateUserPostReaction,
+} from '../api/reactionData';
+// import { postReaction } from '../api/reactionData';
 
 export default function ReactionCard({ Obj, postObj }) {
   const [singleReact, setSingleReact] = useState([]);
   const [singlePost, setSinglePost] = useState([]);
-  // const [singleUser, setSingleUser] = useState([]);
+  const [singleUser, setSingleUser] = useState();
+  const [postReaction, setPostReaction] = useState();
+  const [showCurrentCount, setShowCurrentCount] = useState([]);
   const { user } = useAuth();
+
+  function getUserId() {
+    // Need this userId to check in postReactions table.
+    getSingleUser(user.uid).then(setSingleUser);
+    console.log('Checking User:', singleUser);
+  }
+
+  const payload = {
+    PostId: singlePost,
+    ReactionId: singleReact,
+    UserId: singleUser?.id,
+  };
+
+  function putOrPostReactions() {
+    if (singleUser?.id === postReaction[0]?.userId) {
+      updateUserPostReaction(postReaction[0]?.id, payload);
+    } else if (singleUser?.id !== postReaction[0]?.userId) {
+      postNewReaction(payload);
+      console.log('New payload to DB set:');
+    }
+  }
+
+  const handleSubmit = () => {
+    if (Obj.id) {
+      putOrPostReactions();
+    }
+    // Disable reaction count below this line
+    if (postObj.id) {
+      getIdCount(singlePost).then(setShowCurrentCount);
+    }
+  };
 
   useEffect(() => {
     if (Obj.id) setSingleReact(Obj.id);
     if (postObj.id) setSinglePost(postObj.id);
-  }, [Obj, user, postObj]);
+    getUserId();
+  }, []);
 
-  // const userPayload = {
-  //   FirstName: user.displayName,
-  //   LastName: user.displayName,
-  //   Email: user.email,
-  //   ProfileImage: user.photoURL,
-  //   Uid: user.uid,
-  // };
-
-  // const testUser = () => {
-  //   checkUser(user.uid).then(setSingleUser);
-  //   console.log('Checking User:', singleUser);
-  //   registerUser(user).then(setSingleUser);
-  //   console.log('Register User:', singleUser);
-  // };
-
-  // Line 35 - 47, is to submit into PostReaction table upon click.
-  // const payload = {
-  //   PostId: singlePost,
-  //   ReactId: singleReact,
-  //   UserId: user.uid,
-  // };
-
-  const handleSubmit = () => {
-    if (Obj.id) {
-      // postReaction(payload);
-      console.log('Post Id:', singlePost, 'React Id:', singleReact, 'User uid:', user.uid);
-    }
-  };
-
-  console.log('This is the singleReact:', singleReact);
+  useEffect(() => {
+    getUserPostReaction(singleUser?.id).then((data) => setPostReaction(data));
+  }, [postReaction, singleUser]);
 
   return (
     <>
@@ -61,12 +75,17 @@ export default function ReactionCard({ Obj, postObj }) {
               objectFit: 'cover',
               borderRadius: '100%',
             }}
+            {...showCurrentCount.length}
           />
         </button>
+        <h1>
+          {showCurrentCount.length}
+        </h1>
       </div>
     </>
   );
 }
+
 ReactionCard.propTypes = {
   Obj: PropTypes.shape({
     id: PropTypes.number.isRequired,
